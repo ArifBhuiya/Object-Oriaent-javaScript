fb52daa4cc3652b21ac9de9dc0e2988cde1d75cb
Arif Bhuiya <arifulislam517@gmail.com>
2019-03-16 14:12:37 +0300

    Bind Call Apply Method is Explore Here....

---
+++ bind-call-apply.js
@@ -0,0 +1,51 @@
+// function printme(){
+//     console.log('Hello, ' + this.name + this.age + this.email);
+// }
+
+// var obj ={
+//     name :'Arif Bhuiya ',
+//     age: 24,
+//     email: ' arif@gmail.com'
+// }
+
+// var obj1 ={
+//     name : 'Miss Bhuiya ',
+//     age : 18,
+//     email: ' bhuiya@gmail.com'
+// }
+
+// var binded = printme.bind(obj);
+// var binded1 = printme.bind(obj1);
+
+// binded();
+// binded1();
+
+// printme.call(obj);
+// printme.call(obj1);
+
+
+// function add(a,b){
+//     return (a+b) * this.c;
+
+// }
+
+// var obj = {
+//     c:3
+// }
+
+// var obj1 = {
+//     c:5
+// }
+
+
+
+//  var res = add.call(obj,10 ,4);
+//  res;
+
+
+//  var res1 = add.apply(obj1,[10,4]);
+//  res;
+
+//  var binded = add.bind(obj);
+//  console.log(binded(10,4));
+ 
